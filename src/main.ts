77d4a56789d167db7c26d6021a5dd9aa3a5f4c05
Brayan-724 <bjqf0724@gmail.com>
2024-05-18 22:22:43 -0700

    b

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { appWindow } from '@tauri-apps/api/window';
 import { register } from '@tauri-apps/api/globalShortcut';
 let ignoreCursorEvents = false; // Variable para almacenar el estado
+// WIN + O  or  CTRL + O
+const shortcut = 'Super+O'; // 'CommandOrControl+O'
 
 (async () => {
-  await register('CommandOrControl+O', async () => {
+  await register(shortcut, async () => {
     ignoreCursorEvents = !ignoreCursorEvents; // Cambiar el estado
     if (ignoreCursorEvents) {
       await appWindow.setIgnoreCursorEvents(true); // Bloquear eventos del cursor
